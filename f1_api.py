bd45848df9093179fc9c34ac7f8e94d4c7ed6310
Arturo <arturotorresdamian1@gmail.com>
2026-06-26 21:40:54 -0700

    Updated the api a little
    just messed around with filtering the session type by type race, and will further filter by meeting_type to ensure the latest race is being displayed

--- f1_api.py
+++ f1_api.py
@@ -4,7 +4,8 @@ import requests
 response = requests.get("https://api.openf1.org/v1/sessions?year=2025")
 data = response.json()
 for session in data:
-     #format_pattern = "%A-%B-%d"
-    print(session['country_name'], session['circuit_short_name'], session['session_name'], session['date_start'])
+     if session['session_type']== "Race":
+          print(session['country_name'], session['circuit_short_name'], session['session_name'], session['date_start'])
+
    
 
