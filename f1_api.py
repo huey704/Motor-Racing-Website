39372ea5d6ecf834d29e7d3eb2d1d989e3ef91ef
Arturo <arturotorresdamian1@gmail.com>
2026-07-02 15:15:08 -0700

    Update f1_api.py
    do this has the correct label style that I need, but Im going to end up restarting in node js using js to finish the rest of the api, and scraping of the the other two racing series

--- f1_api.py
+++ f1_api.py
@@ -1,11 +1,24 @@
 import datetime
 import requests
 
-response = requests.get("https://api.openf1.org/v1/sessions?year=2025")
+response = requests.get("https://api.openf1.org/v1/sessions?meeting_key=latest&year=2026")
 data = response.json()
+
+first = True
 for session in data:
-     if session['session_type']== "Race":
-          print(session['country_name'], session['circuit_short_name'], session['session_name'], session['date_start'])
+    if first:
+        print(session["circuit_short_name"],":", session["country_name"])
+        first = False
+
+    if session['session_type']== "Practice":
+          print(session['session_name'],":", session['date_start'])
+    
+    elif session['session_type']== "Qualifying":
+        print(session['session_name'],":", session['date_start'])
+
+    elif session['session_type']== "Race":
+        print(session['session_name'],":", session['date_start'])
 
    
+
 
