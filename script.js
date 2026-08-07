fd66a7f28815682398892409fc92446f701469f7
Arturo <arturotorresdamian1@gmail.com>
2026-08-06 21:54:33 -0700

    Added FP1 to FP3 live data to the website

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ document.querySelectorAll('.blackout-button').forEach(button => {
     });
 });
 
-const trackLocation = document.querySelector('#f1-trackLocation');
+const f1TrackLocation = document.querySelector('#f1-TrackLocation');
+const f1Fp1 = document.querySelector('#f1-Fp1');
+const f1Fp2 = document.querySelector('#f1-Fp2');
+const f1Fp3 = document.querySelector('#f1-Fp3');
 
 async function loadSessions() {
     try {
@@ -13,12 +16,13 @@ async function loadSessions() {
         const response = await fetch("https://api.openf1.org/v1/sessions?meeting_key=latest&year=2026");
         const data = await response.json();
 
-        console.log(
-            data[0].circuit_short_name,
-            data[0].country_name
+        console.log(data[0])
 
-        );
-        trackLocation.textContent = "Track: " + data[0].circuit_short_name + ", " + data[0].country_name;
+        f1TrackLocation.textContent = "Track: " + data[0].circuit_short_name + ", " + data[0].country_name;
+        f1Fp1.textContent = "FP1: " + data[0].date_start;
+        f1Fp2.textContent = "FP2: " + data[1].date_start;
+        f1Fp3.textContent = "FP3: " + data[2].date_start;
+
     } catch (error) {
         console.error("Failed:", error);
     }
